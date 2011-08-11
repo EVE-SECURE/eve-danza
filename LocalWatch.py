acdfcddfebd1bd1f3350c7c47425a789d5dcaf4f
billchang.e@gmail.com <billchang.e@gmail.com@f3914bce-36e1-3eec-4c13-256045132cca>
2011-08-11 19:52:07 +0000

    LocalWatch.py updated to include real time update and warning (see LocalDash and WarningFlash), both are toggled with buttons on neocom. Also fixed various bugs.

--- LocalWatch.py
+++ LocalWatch.py
@@ -35,6 +35,7 @@ try:
 	import spaceObject
 	import blue
 	import timecurves
+	import copy
 
 	try:
 		form.Scanner.localChannel = None
@@ -239,15 +240,28 @@ try:
 
 		def __init__(self):
 			service.Service.__init__(self)
-			self.alive = base.AutoTimer(1000, self.Update)
-			sm.GetService('gameui').Say('MyService inited')
+			sm.GetService('gameui').Say('LocalWatch Service started')
 			self.pane = None
-			self.A_down = False
-			self.S_down= False
-			self.D_down= False
-			self.W_down= False
+			self.cache = {}
+			self.hostilecount = -1
+			self.busy = False
+			self.localChannel = None
+			self.warning = False
+			# search for local channel window and save it
+			# WARNING: currently we do not update this reference on session change, add it in later!
+			for channelID in sm.GetService('LSC').channels.keys():
+				channel = sm.GetService('LSC').channels[channelID]
+				if (channel.window and ((type(channel.channelID) is tuple) and (channel.channelID[0][0] == 'solarsystemid2'))):
+					self.localChannel = channel
+  			self.alive = base.AutoTimer(1000, self.Update)
+
+##			self.A_down = False
+##			self.S_down= False
+##			self.D_down= False
+##			self.W_down= False
 
 		def initPane(self):
+			"""
 			self.pane = sm.GetService('window').GetWindow("Temp", create=1)
 			self.pane.windowID = "TempWindow"
 			btn = uix.GetBigButton(50, self.pane.sr.main, left=0)
@@ -261,6 +275,15 @@ try:
 			btn.hint = "TryWarpToSafe"
 			btn.sr.icon.LoadIcon('44_01')
 			self.pane.sr.Focus2Btn = btn
+			"""
+			self.pane = LocalDash(parent=uicore.layer.abovemain, name='LocalDash')
+			#self.pane.ShowMsg("<color=0xff0037ff>BLUES</color> "+" <color=0xffff3700>REDS</color> "+" <color=0xffffffff>TOTAL</color>" + "<br>",
+			#				"<color=0xff0037ff>0</color>    "+" <color=0xffff3700>0</color> "+"    <color=0xffffffff>0</color>")
+			if self.hostilecount == -1 or self.busy:
+				self.pane.ShowMsg("<color=0xffffffff>HOSTILES: " + "<color=0xffff3700>updating...</color>")
+			else:
+				self.pane.ShowMsg("<color=0xffffffff>HOSTILES: " + "<color=0xffff3700>%d</color>" % self.hostilecount)
+
 
 		def Update(self):
 
@@ -284,7 +307,74 @@ try:
 				ret += ' W'
 			msg(ret)
 			"""
-			pass
+			if not self.busy:
+				# safe to update hostile count since we're not busy
+				self.hostilecount = self.GetHostileCount()
+				# update pane only if we have it open
+			if self.pane:
+				self.pane.Close()
+ 				self.pane = LocalDash(parent=uicore.layer.abovemain, name='LocalDash')
+				if self.hostilecount == -1 or self.busy:
+					self.pane.ShowMsg("<color=0xffffffff>HOSTILES: " + "<color=0xffff3700>updating...</color>")
+				else:
+					self.pane.ShowMsg("<color=0xffffffff>HOSTILES: " + "<color=0xffff3700>%d</color>" % self.hostilecount)
+			if self.warning and (self.hostilecount > 0):
+				flash = WarningFlash(parent=uicore.layer.abovemain, name='WarningFlash')
+				flash.Flash()
+
+		def GetHostileCount(self):
+			# set service status to busy to avoid lock
+			self.busy = True
+			if self.hostilecount == -1:
+				self.hostilecount = 0
+			count = 0
+			mlist = copy.deepcopy(self.localChannel.memberList)
+			for charID in mlist:
+				# if it's myself, skip it
+				if charID == session.charid:
+					continue
+				else:
+					# check cache first
+					if charID in self.cache.keys():
+						if self.cache[charID] < 0.5:
+							# increment counter if less than friendly
+							count += 1
+							#msg('hostiles: %d' % count)
+					# charID not in cache, we need to find out the standing the hard way
+					else:
+						# figure out what ID to use for myself
+						myStandingID = None
+						if (eve.session.allianceid):
+							myStandingID = eve.session.allianceid
+						else:
+							myStandingID = eve.session.corpid
+						standing = sm.GetService('standing').GetStanding(myStandingID, charID)
+						if (standing == 0.0):
+							# neutral standing could mean charID is in our alliance or corp
+							corpCharInfo = sm.GetService('corp').GetInfoWindowDataForChar(charID, 1)
+							corpID = corpCharInfo.corpID
+							allianceID = corpCharInfo.allianceID
+							if (eve.session.corpid == corpID) or ((eve.session.allianceid != None) and (eve.session.allianceid == allianceID)):
+								# charID is in our alliance or corp, mark friendly in cache and skip
+								self.cache[charID] = 1.0
+								continue
+							else:
+								# charID is not in our alliance or corp, better mark hostile in cache and increment count
+								self.cache[charID] = standing
+								count += 1
+								#msg('hostiles: %d' % count)
+						elif (standing < 0.5):
+							# we found a new hostile, store in cache and increment counter
+							self.cache[charID] = standing
+							count += 1
+							#msg('hostiles: %d' % count)
+						else:
+							# charID is in a friendly alliance/corp or tagged friendly by alliance, store in cache and skip
+							self.cache[charID] = standing
+							continue
+			# we're done, no longer busy
+			self.busy = False
+			return count
 
 		def Open(self):
 			if self.pane:
@@ -299,29 +389,89 @@ try:
 				self.pane.Close()
 				del self.pane
 				self.pane = None
+
+		def WarningOn(self):
+			self.warning = True
+
+		def WarningOff(self):
+			self.warning = False
+
 		def CleanUp(self):
 			del self.alive
 			self.alive = None
-			del self.pane
-			self.pane = None
+			self.Reset()
+
+	class WarningFlash(uicls.Container):
+			__guid__ = 'uicls.Warning'
+
+			def ApplyAttributes(self, attributes):
+				self.scope = 'station_inflight'
+				self.frame = None
+				uicls.Container.ApplyAttributes(self, attributes)
+
+			def Flash(self, duration = 500):
+				self.SetAlign(uiconst.CENTERTOP)
+				self.SetSize(uicore.desktop.width, uicore.desktop.height)
+				self.frame = uicls.Frame(parent=self, color=(1.0, 0.25, 0.25, 0.25), frameConst=uiconst.FRAME_FILLED_CORNER1, state=uiconst.UI_DISABLED)
+				self.SetPosition(0, 0)
+				self.state = uiconst.UI_DISABLED
+				uiutil.SetOrder(self, 0)
+				blue.pyos.synchro.Sleep(duration)
+				self.Close()
+
+
+	class LocalDash(uicls.Container):
+			__guid__ = 'uicls.LocalDash'
+
+			def ApplyAttributes(self, attributes):
+				self.scope = 'station_inflight'
+				self.message = None
+				#self.message2 = None
+				uicls.Container.ApplyAttributes(self, attributes)
+
+			def Prepare_Text_(self):
+				self.message = uicls.Label(text='', parent=self, left=0, top=4, autowidth=False, width=100, fontsize=12, state=uiconst.UI_DISABLED)
+				#self.message2 = uicls.Label(text='', parent=self, left=0, top=16, autowidth=False, width=200, fontsize=12, state=uiconst.UI_DISABLED)
+
+			def Prepare_Underlay_(self):
+			    border = uicls.Frame(parent=self, frameConst=uiconst.FRAME_BORDER1_CORNER5, state=uiconst.UI_DISABLED, color=(1.0, 1.0, 1.0, 0.25))
+			    frame = uicls.Frame(parent=self, color=(0.0, 0.0, 0.0, 0.75), frameConst=uiconst.FRAME_FILLED_CORNER1, state=uiconst.UI_DISABLED)
+
+			def ShowMsg(self, text1):
+			    if self.message is None:
+			        self.Prepare_Text_()
+			        self.Prepare_Underlay_()
+				self.message.text = '<center>' + text1
+				#self.message2.text = '<center>' + text2
+				self.SetAlign(uiconst.CENTERTOP)
+				self.SetSize(100, 20)
+				offset = sm.GetService('window').GetCameraLeftOffset(self.width, align=uiconst.CENTERTOP, left=0)
+				self.SetPosition(offset, 5)
+				self.state = uiconst.UI_DISABLED
+				uiutil.SetOrder(self, 0)
+
+
 
 	@safetycheck
 	def CreateIt(*args):
 		#create an instance of something
 		bottomline = sm.GetService('neocom').bottomline
 		if bottomline and hasattr(bottomline, "alive") and bottomline.alive:
-			msg('MyService already running!')
+			msg('LocalWatch Service already running!')
 		else:
 			sm.GetService('neocom').bottomline = MyService()
 
 	@safetycheck
 	def DestroyIt(*args):
 		#destroy an instance of something
+		if sm.GetService('neocom').bottomline == None:
+			msg('LocalWatch Service not running!')
+			return
 		if hasattr(sm.GetService('neocom').bottomline, 'alive'):
 			sm.GetService('neocom').bottomline.CleanUp()
 		del sm.GetService('neocom').bottomline
 		sm.GetService('neocom').bottomline = None
-		msg('MyService killed!')
+		msg('LocalWatch Service killed!')
 
 	@safetycheck
 	def ToggleIt(*args):
@@ -331,6 +481,17 @@ try:
 				bottomline.Close()
 			else:
 				bottomline.Open()
+
+	@safetycheck
+	def WarnIt(*args):
+		bottomline = sm.GetService('neocom').bottomline
+		if bottomline and hasattr(bottomline, 'alive') and bottomline.alive:
+			if bottomline.warning:
+				bottomline.WarningOff()
+				msg('Warning toggled OFF!')
+			else:
+				bottomline.WarningOn()
+				msg('Warning toggled ON!')
 
 	@safetycheck
 	def TryGetInvItem(itemID):
@@ -392,21 +553,27 @@ try:
 		neocomwnd = sm.GetService('neocom').main
 		btn = uix.GetBigButton(32, neocomwnd, top=800)
 		btn.OnClick = CreateIt
-		btn.hint = ""
+		btn.hint = "Start LocalWatch service"
 		btn.sr.icon.LoadIcon('11_01')
 		createBtn = btn
 
 		btn = uix.GetBigButton(32, neocomwnd, top=833)
 		btn.OnClick = DestroyIt
-		btn.hint = ""
+		btn.hint = "Kill LocalWatch service"
 		btn.sr.icon.LoadIcon('11_02')
 		destroyBtn = btn
 
 		btn = uix.GetBigButton(32, neocomwnd, top=866)
 		btn.OnClick = ToggleIt
-		btn.hint = ""
+		btn.hint = "Show LocalDash"
 		btn.sr.icon.LoadIcon('11_03')
 		killBtn = btn
+
+		btn = uix.GetBigButton(32, neocomwnd, top=899)
+		btn.OnClick = WarnIt
+		btn.hint = "Toggle flash warning"
+		btn.sr.icon.LoadIcon('11_04')
+		warnBtn = btn
 
 
 	except:
